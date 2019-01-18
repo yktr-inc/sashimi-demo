1088f74238c0d82551996e31097529fccc97d9a6
Louis Harang <vundaboy@gmail.com>
2019-01-18 17:08:39 +0100

    adding nodemon

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const http = require('http');
 const fs = require('fs');
 const index = fs.readFileSync('src/static/index.html');
 
-http.createServer( (req, res) => {
-  console.log(process.cwd());
+http.createServer( function(req, res) {
+  console.log("Sashimi app is running !");
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(index);
+  res.write(index);
+  res.end();
 }).listen(8080);
